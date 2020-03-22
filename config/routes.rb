47bd3fe59c7f4090377e69ea5b46db28e5283053
takuma10Mi <takumaii@yahoo.co.jp>
2020-03-22 09:41:40 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'blogs#index'
   resources :blogs
-  post '/complete' => 'blogs#create'
-  post '/login' => 'blogs#login'
-  get '/:id/edit' => 'blogs#edit'
-  patch '/:id' => 'blogs#update'
-  get '/new' => 'blogs#new'
+  patch 'blogs/:id'
   resources :blogs do
     collection do
       # デフォルト形式をcsvに設定
